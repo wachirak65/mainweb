4eefb7a5107c36e6b980866600d9ecc6133a1de3
Sitta Sahathum <sittasahathum@gmail.com>
2023-11-08 03:29:09 +0700

    add nav-bar-v-1

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -1,22 +1,23 @@
 // Navbar.js
-import React from 'react';
+import React, { useState } from 'react'; // Import useState from React
 import logo_untired from '../assets/logo_untired.PNG';
 import person_icon from '../assets/icons/person_icon.svg'
 import gear_icon from '../assets/icons/gear_icon.svg'
 import './navbar.css'
 
 function Navbar() {
+    
+    const [showMobileMenu, setShowMobileMenu] = useState(false);
+  
+    const toggleMobileMenu = () => {
+      setShowMobileMenu(!showMobileMenu);
+    };
     return (
         <nav class="navbar">
                     <div class="first-navbar">
                         <a class="navbar-logo" href="#">
                             <img src={logo_untired} alt="Logo" class="logo" />
                         </a> 
-                        <div class = "nav-burgur">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
-                                    <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
-                                </svg>
-                                </div>
                     </div> 
                     
                     <div class="last-navbar">
@@ -49,7 +50,42 @@ function Navbar() {
                             </svg>
                         </div>
                     </div>
-                </nav> 
+                    <div className="nav-burger" id='nav-burger-bar'  onClick={toggleMobileMenu}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+                            </svg>
+                    </div>
+                    {showMobileMenu && (
+                        <div className="mobile-menu">
+                        <ul>
+                            <li><p>Project</p></li>
+                            <li><div class = 'Block-for-project'>
+                                
+                                Block for Project
+                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus corporis in qui velit deserunt ratione vitae dolores, perspiciatis quis asperiores, culpa tempore, nemo magnam nobis labore eos numquam accusamus repudiandae veniam! Voluptatibus aperiam fugit nam inventore exercitationem ipsa harum iusto dolores pariatur ad sapiente dolor molestias sequi, dolorem architecto facilis mollitia itaque laborum magnam. Mollitia expedita id ad, aut rerum itaque? Officiis alias sint maxime omnis quidem, vitae velit minima iusto doloribus eos tenetur obcaecati quasi. Itaque nulla consequatur veniam. Nemo consequatur aperiam earum unde. Ex ut itaque delectus eligendi odio odit laborum dolorem iure porro consequatur vitae, soluta iste architecto, excepturi expedita mollitia. Distinctio culpa saepe laudantium ab illo, omnis et autem exercitationem enim molestias non minima, veritatis laboriosam, commodi tempora. Sint laboriosam non possimus hic, tenetur rem molestias iure, officiis voluptatibus, qui ipsum rerum maxime accusantium quisquam! Tenetur iusto nam, necessitatibus iure explicabo animi ducimus, velit perferendis provident soluta saepe sunt minima? Earum, aspernatur? Ex, voluptate reiciendis dolores commodi culpa et nostrum earum fugiat expedita debitis nisi possimus voluptatem est porro vero! Dolorem quis nesciunt animi modi ipsum? Soluta dolorum ut hic sapiente eos tempora, nostrum corporis. Dicta architecto, inventore voluptatibus eligendi fugit deserunt ut cum beatae sequi.
+                                </div>
+                            </li>
+                            <div class = "ground-setting-1">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"/></svg>                                 
+                                <p>โปรไฟล์</p>
+                            </div>
+                            <div class = "ground-setting-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-bell" viewBox="0 0 16 16"><path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z"/></svg>
+                                <p>แจ้งเตือน</p>
+                            </div>
+                            <div class = "ground-setting-3">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-gear" viewBox="0 0 16 16"><path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z"/><path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z"/></svg>
+                                <p>การตั้งค่า</p>
+                            </div>
+                            <p id='other-discript'>เพิ่มเติม</p>
+                            <li><a href="#">เกี่ยวกับเรา</a></li>
+                            <li><a href="#">เทคโนโลยี AI</a></li>
+                            <li><a href="#">บริการ</a></li>
+                            <li><a href="#">ติดต่อเรา</a></li>
+                        </ul>
+                        </div>
+                    )}
+    </nav> 
     );
 }
 
